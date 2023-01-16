f500c4c405645983344ad02321558ab79bea5b06
coresoft1410 <salmankhan1410khan@gmail.com>
2023-01-17 00:43:29 +0530

    about us and beautification changes

--- src/app/view-profile/view-profile-routing.module.ts
+++ src/app/view-profile/view-profile-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 import { PayFeesComponent } from './pay-fees/pay-fees.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { RaiseQueryComponent } from './raise-query/raise-query.component';
@@ -11,7 +13,9 @@ const routes: Routes = [
   {path: 'view_profile', component: PersonalInfoComponent},
   {path: 'result', component: ResultComponent},
   {path: 'payfees', component:PayFeesComponent},
-  {path: 'raise', component:RaiseQueryComponent}
+  {path: 'raise', component:RaiseQueryComponent},
+  {path: 'contactus', component:ContactusComponent},
+  {path: 'aboutus', component:AboutusComponent}
 ];
 
 @NgModule({
